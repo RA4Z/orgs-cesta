c0599d6e6a6d5a20a3e3bfd95f84f087de10299c
RA4Z <rraz639@gmail.com>
2023-04-21 14:54:10 -0300

    Criação de lista usando map

--- src/telas/cesta/index.js
+++ src/telas/cesta/index.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 import Detalhes from "./componentes/Detalhes";
+import Itens from "./componentes/Itens";
 import Topo from "./componentes/Topo";
 
 
-function Cesta({topo, detalhes}) {
-    return <>  
+function Cesta({topo, detalhes, itens}) {
+    return <ScrollView>  
     <Topo {...topo}/>
         <View style={estilos.cesta}>
             <Detalhes {...detalhes} />
+            <Itens {...itens}/>
         </View>
             
-    </>
+    </ScrollView>
 }
 
 const estilos = StyleSheet.create({
