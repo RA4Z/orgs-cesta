b8e6e4a01f97078341ab411a6a11983ad6489046
RA4Z <rraz639@gmail.com>
2023-04-21 14:34:45 -0300

    Desafio: fazer botão se tornar um componente

--- src/telas/cesta/componentes/Detalhes.js
+++ src/telas/cesta/componentes/Detalhes.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 
 import Texto from "../../../componentes/Texto";
+import Botao from "../../../componentes/Botao"
 
 export default function Detalhes({nome, nomeFazenda, logoFazenda, descricao, preco, botao}) {
     return <>
@@ -12,10 +13,7 @@ export default function Detalhes({nome, nomeFazenda, logoFazenda, descricao, pre
         </View>
         <Texto style={estilos.descricao}>{ descricao }</Texto>
         <Texto style={estilos.preco}>{ preco }</Texto>
-
-        <TouchableOpacity style={estilos.botao} onPress={() => {}}>
-            <Texto style={estilos.textoBotao}>{ botao }</Texto>
-            </TouchableOpacity>
+        <Botao>{ botao }</Botao>
     </>
 }
 
@@ -51,18 +49,5 @@ const estilos= StyleSheet.create({
         fontSize: 26,
         lineHeight: 42,
         marginTop: 8,
-    },
-    botao: {
-        marginTop: 16,
-        backgroundColor: '#2A9F85',
-        paddingVertical: 16,
-        borderRadius: 6,
-    },
-    textoBotao: {
-        textAlign: 'center',
-        color: '#ffffff',
-        fontSize: 16,
-        lineHeight: 26,
-        fontWeight: 'bold',
     },
 })
