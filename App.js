b89f590538f9e3531f38743d72ba7cac39c6f46f
RA4Z <rraz639@gmail.com>
2023-04-21 14:09:00 -0300

    Uso de Splash Screen para esperar carregamento

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StatusBar, SafeAreaView, View } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from "@expo-google-fonts/montserrat";
 
+import * as SplashScreen from 'expo-splash-screen';
+
 import mock from './src/mocks/cesta'
 import Cesta from './src/telas/cesta';
 
@@ -11,8 +13,14 @@ export default function App() {
     "MontserratBold": Montserrat_700Bold,
   })
 
+  SplashScreen.preventAutoHideAsync()
+    .then(() => {
+      // Suas ações aqui, como carregar dados iniciais ou redirecionar o usuário
+    })
+    .finally(() => SplashScreen.hideAsync());
+
   if(!fonteCarregada) {
-    return <View />
+    return null
   }
 
   return (
