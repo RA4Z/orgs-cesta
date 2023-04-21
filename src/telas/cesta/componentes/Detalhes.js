66f6d1e7b7001b4183c9d2e5f5938b710ee444e7
RA4Z <rraz639@gmail.com>
2023-04-21 14:20:52 -0300

    Criação de Botão

--- src/telas/cesta/componentes/Detalhes.js
+++ src/telas/cesta/componentes/Detalhes.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
 
 import Texto from "../../../componentes/Texto";
 
-export default function Detalhes({nome, nomeFazenda, logoFazenda, descricao, preco}) {
+export default function Detalhes({nome, nomeFazenda, logoFazenda, descricao, preco, botao}) {
     return <>
         <Texto style={estilos.nome}>{ nome }</Texto>
-            <View style={estilos.fazenda}>
-                <Image source={ logoFazenda } style={estilos.imagemFazenda} />
-                <Texto style={estilos.nomeFazenda}>{ nomeFazenda }</Texto>
-            </View>
-            <Texto style={estilos.descricao}>{ descricao }</Texto>
-            <Texto style={estilos.preco}>{ preco }</Texto>
+        <View style={estilos.fazenda}>
+            <Image source={ logoFazenda } style={estilos.imagemFazenda} />
+            <Texto style={estilos.nomeFazenda}>{ nomeFazenda }</Texto>
+        </View>
+        <Texto style={estilos.descricao}>{ descricao }</Texto>
+        <Texto style={estilos.preco}>{ preco }</Texto>
+
+        <TouchableOpacity style={estilos.botao} onPress={() => {}}>
+            <Texto style={estilos.textoBotao}>{ botao }</Texto>
+            </TouchableOpacity>
     </>
 }
 
@@ -47,5 +51,18 @@ const estilos= StyleSheet.create({
         fontSize: 26,
         lineHeight: 42,
         marginTop: 8,
-    }
+    },
+    botao: {
+        marginTop: 16,
+        backgroundColor: '#2A9F85',
+        paddingVertical: 16,
+        borderRadius: 6,
+    },
+    textoBotao: {
+        textAlign: 'center',
+        color: '#ffffff',
+        fontSize: 16,
+        lineHeight: 26,
+        fontWeight: 'bold',
+    },
 })
